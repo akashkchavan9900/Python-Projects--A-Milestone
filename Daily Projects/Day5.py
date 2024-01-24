3c912b9a8b48926170917e0d939a5735ba9afb52
Akash Chavan <akashkchavan9900@gmail.com>
2024-01-24 20:32:18 +0530

    Password Generator

---
+++ Daily Projects/Day5.py
@@ -0,0 +1,61 @@
+#Day 5: Keep things Private.
+#Topics: Loops-FOR 
+#Project: Password Generator 
+""" Problem Statement"""
+# To build a Randomised Passoword Generator.
+# Use Random Module and its functions
+"""-------------Code-------------"""
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', ' C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the Password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level: Order of characters Not Randomised
+password = ""
+
+for char in range(1, nr_letters + 1):
+  password += random.choice(letters)
+
+for char in range(1, nr_symbols + 1):
+  password += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password += random.choice(numbers)
+
+print(f'Your password is"(not randomised)": {password}')
+
+#Hard Level: Order of characters Randomised:
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f'Your password is"(randomised)": {password}')
+
+""""-----------------------Output-----------------"""
+# Welcome to the Password Generator!
+# How many letters would you like in your password?
+# 4
+# How many symbols would you like?  
+# 2
+# How many numbers would you like?
+# 2
+# Your password is"(nor randomised)": nnga)(06
+# Your password is"(Randomised)": 5cvvT$4&
