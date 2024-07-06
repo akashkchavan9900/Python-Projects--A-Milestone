b6d87fc55e55f771742737c07f0bf6aa648d7980
Akash Chavan <akashkchavan9900@gmail.com>
2024-07-06 22:36:57 +0530

    06: Escaping the Maze

--- Daily Projects/Day6.py
+++ Daily Projects/Day6.py
@@ -1,6 +1,6 @@
 #Day 6: 
 #Topics: Defining Function & While  Loop
-#Project: Escaping the Maze
+#Project: Escaping the Maze 
 """ Problem Statement""" 
 # Link: https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 # Run the below code on the given above link.
